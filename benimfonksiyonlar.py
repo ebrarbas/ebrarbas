672c8da5c5dcd7d005d24672ca0a36dcc3fbf55d
ebrarbas <115706439+ebrarbas@users.noreply.github.com>
2022-11-25 12:44:43 +0300

    Add files via upload

---
+++ benimfonksiyonlar.py
@@ -0,0 +1,16 @@
+# def selam_soyle() :
+#     print("merhaba")
+
+# selam_soyle()
+
+# import math
+
+# def final_hesapla(vize):
+#     print
+
+# vize=int(input("vize sonucunu giriniz:"))
+# if vize<50 :
+#     final_için_gerekli_not=(50-0.4*vize)/0.6
+#     print("final için almanız gereken not:",math.ceil(final_için_gerekli_not))
+# else :
+#    print("final için almanız gereken not:50")
---
+++ besinci.py
@@ -0,0 +1,72 @@
+#  absolute path vs relative path
+# tam yol veya bağıl yol
+# ...üst klasörü gösterir...
+
+# f = open("test.txt")
+# print( f.read())
+# f.close()
+
+# import random as rnd 
+
+# print(rnd.randrange(0,100))
+# print(rnd.random())
+
+# def selam_soyle() :
+#     print("merhaba")
+
+# import benimfonksiyonlar
+# benimfonksiyonlar.selam_soyle()
+
+# import benimfonksiyonlar yazınca tamamen dosyayı çağırır
+# aşağıdakini yazınca sadece selam_soylefonksiyonunu çağırır
+# from benimfonksiyonlar import selam_soyle
+
+# def selam_soyle ():
+#     print("besinci py içindem..")
+
+# benimfonksiyonlar.selam_soyle()
+
+# final_hesapla(20)
+
+# import mymodule.oto
+
+# print(mymodule.oto.version)
+# mymodule.oto.ecu_ver()
+
+import math
+
+# print(math.pow(2,10))
+# print(math.sqrt(25))
+# print(math.pi)
+# print(math.log(2))
+# log 3 tabanında 5 nedir
+# print(math.log(5,3))
+
+
+# if __name__== "__main__" :
+#     print("python yorumlayıcısının çağırdığı dosya içerisindesiniz.")
+
+
+# dosyalama çokönemli
+# modul
+# data structures
+
+import math
+a=3
+b=math.pi
+c="001"
+d=5.2
+e:1_000_000
+# print("hata yaz")
+# print(a" ","pi="b,c,d,sep="")
+# print(   "d:{1} pi={0:.3f}".format(b,d)   )
+
+print(  "{0:0>7}\t{0:#>7}\t{0:x>7}".format(a)  )
+print("{0}\t{0}\t{0)".format(e))
+
+
+
+
+
+
+
